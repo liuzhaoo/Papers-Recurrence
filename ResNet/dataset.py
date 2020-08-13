69be1db51cad544f94f561365259cc9a4fe744e0
liuzhao <415091lz@gmail.com>
2020-08-13 17:35:40 +0800

    finash dataset

--- ResNet/dataset.py
+++ ResNet/dataset.py
@@ -3,8 +3,115 @@ import os
 import os.path
 import numpy as np
 import pickle
+import hashlib
+
+def calculate_md5(fpath, chunk_size=1024 * 1024):
+    md5 = hashlib.md5()
+    with open(fpath, 'rb') as f:
+        for chunk in iter(lambda: f.read(chunk_size), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
+def check_md5(fpath, md5, **kwargs):
+    return md5 == calculate_md5(fpath, **kwargs)
 
 
+def check_integrity(fpath, md5=None):
+    if not os.path.isfile(fpath):
+        return False
+    if md5 is None:
+        return True
+    return check_md5(fpath, md5)
 
 class cifa10():
-    def __init__(self,):
+    base_folder = 'cifar-10-batches-py'
+    train_list = [
+        ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
+        ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
+        ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
+        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
+        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
+    ]
+    test_list = [
+        ['test_batch', '40351d587109b95175f43aff81a1287e'],
+    ]
+    meta = {
+        'filename': 'batches.meta',
+        'key': 'label_names',
+        'md5': '5ff9c542aee3614f3951f8cda6e48888',
+    }
+    def __init__(self, root,train=True,transform=None, target_transform=None,):
+        super(cifa10, self).__init__()
+        self.train = train
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+        if self.train:
+            data_list = self.train_list
+        else:
+            data_list = self.test_list
+
+        self.data = []
+        self.targets = []
+
+        for file_name,checksum in data_list:
+            filepath = os.path.join(self.root,self.base_folder,file_name)
+            with open(filepath,'rb') as f:
+                entry = pickle.load(f,encoding='latin')                    # 读取文件，得到的结果为dict_keys(['batch_label', 'labels', 'data', 'filenames'])
+                self.data.append(entry['data'])                            # 取‘data’对应的值，　10000 * 3072 的二维数组，每一行代表一张图片的像素值。（32*32*3=3072）
+                if 'labels' in entry:
+                    self.targets.extend(entry['labels'])
+                else:
+                    self.targets.extend(entry['fine_labels'])              #取lables 对应的值
+
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
+
+        self._load_meta()
+
+    def _load_meta(self):
+        path = os.path.join(self.root, self.base_folder, self.meta['filename'])
+        if not check_integrity(path, self.meta['md5']):
+            raise RuntimeError('Dataset metadata file not found or corrupted.' +
+                               ' You can use download=True to download it')
+        with open(path, 'rb') as infile:
+            data = pickle.load(infile, encoding='latin1')               # read  batches.meta
+            self.classes = data[self.meta['key']]                       # get label_names ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+            self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)} # 将class与0-10 对应
+    def __getitem__(self, index):
+        """
+                Args:
+                    index (int): Index
+
+                Returns:
+                    tuple: (image, target) where target is index of the target class.
+        """
+
+        img = self.data[index]
+        target = self.targets[index]
+
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def _check_integrity(self):
+        root = self.root
+        for fentry in (self.train_list + self.test_list):
+            filename, md5 = fentry[0], fentry[1]
+            fpath = os.path.join(root, self.base_folder, filename)
+            if not check_integrity(fpath, md5):
+                return False
+        return True
