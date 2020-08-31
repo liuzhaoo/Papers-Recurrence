cfd6282cd4f2d0b4d7ca2c63acc1f7eb43dee55d
liuzhao <415091lz@gmail.com>
2020-08-31 09:58:11 +0800

    worked

--- 3DResNet/utils/kinetics_json.py
+++ 3DResNet/utils/kinetics_json.py
@@ -3,12 +3,14 @@ import json
 from pathlib import Path
 
 import pandas as pd
+from joblib import Parallel, delayed
+import re
 # from utils import get_n_frames
 
 
 def get_n_frames(video_path):
 	return len([
-		x for x in video_path.iterdir()     # 在video子目录中循环
+		x for x in video_path.iterdir()  # 在video子目录中循环
 		if 'image' in x.name and x.name[0] != '.'
 	])
 
@@ -21,9 +23,14 @@ def convert_csv_to_dict(csv_path):
 	for i in range(data.shape[0]):  # 0即代表行
 		row = data.iloc[i, :]  # 取一行的所有内容
 		# 得到
-		basename = '%s' % row["youtube_id"]  #
-		keys.append(basename)
+		if subset == 'train':
+			basename = '%s_%s_%s' % (row['youtube_id'], '%06d' % row['time_start'],
+			                         '%06d' % row['time_end'])
+			keys.append(basename)
 
+		elif subset == 'validate':
+			basename = '%s' % row["youtube_id"]  #
+			keys.append(basename)
 		if subset != 'testing':
 			key_labels.append(row['label'])
 
@@ -47,19 +54,20 @@ def load_labels(train_csv_path):
 	return data['label'].unique().tolist()  # 将所有类放到列表里
 
 
-def convert_kinetics_csv_to_json(train_csv_path, val_csv_path,
-								 video_dir_path, video_type, dst_json_path):
-	labels = load_labels(train_csv_path)
-	train_database = convert_csv_to_dict(train_csv_path)
-	val_database = convert_csv_to_dict(val_csv_path)
-	# if test_csv_path.exists():
-	# 	test_database = convert_csv_to_dict(test_csv_path)
-
+def convert_kinetics_csv_to_json(trainval_csv_path,
+                                 video_dir_path, video_type, dst_json_path):
+	labels = load_labels(trainval_csv_path)
 	dst_data = {}
 	dst_data['labels'] = labels
 	dst_data['database'] = {}
-	dst_data['database'].update(train_database)
-	dst_data['database'].update(val_database)
+	# train_database = convert_csv_to_dict(train_csv_path)
+	# val_database = convert_csv_to_dict(val_csv_path)
+	# if test_csv_path.exists():
+	# 	test_database = convert_csv_to_dict(test_csv_path)
+
+	database = convert_csv_to_dict(train_csv_path)
+	# dst_data['database'].update(train_database)
+	dst_data['database'].update(database)
 	# if test_csv_path.exists():
 	# 	dst_data['database'].update(test_database)
 	non_exist_id = []
@@ -69,16 +77,17 @@ def convert_kinetics_csv_to_json(train_csv_path, val_csv_path,
 		else:
 			label = 'test'
 
+		label = re.sub(r' ','_',label)         # 将类别中的空格改为 _ 
+
 		if video_type == 'jpg':
 			video_path = video_dir_path / label / k
-			if video_path.exists():
+			if video_path.is_dir():
 				n_frames = get_n_frames(video_path)  # 计算帧数量
 				v['annotations']['segment'] = (1, n_frames + 1)
 			else:
 				non_exist_id.append(k)
-				break
+
 	for i in non_exist_id:
-		print(i)
 		x = str(i)
 		dst_data['database'].pop(x)
 	with dst_json_path.open('w') as dst_file:
@@ -92,21 +101,21 @@ if __name__ == '__main__':
 
 	)
 	parser.add_argument(
-		'--video_path', default='/home/lzhao/文档/datasets/kinetics/train_jpg', type=Path, help='照片数据'
+		'--video_path', default='/home/lzhao/FILE/datasets/kinetics/train_jpg', type=Path, help='照片数据'    # 每次修改
 	)
 	parser.add_argument('--video_type',
-						default='jpg',
-						type=str,
-						help=('jpg or hdf5'))
+	                    default='jpg',
+	                    type=str,
+	                    help=('jpg or hdf5'))
 	parser.add_argument('--dst_path',
-						default='../testvideo/kinetics400/json',
-						type=Path,
-						help='Path of dst json file.')
+	                    default='../testvideo/kinetics400/train_jpg.json',
+	                    type=Path,
+	                    help='Path of dst json file.')
 	args = parser.parse_args()
 
 	train_csv_path = (args.dir_path / 'train.csv')
 	val_csv_path = (args.dir_path / 'validate.csv')
 	test_csv_path = None
 
-	convert_kinetics_csv_to_json(train_csv_path, val_csv_path,
-								 args.video_path, args.video_type,args.dst_path)
+	convert_kinetics_csv_to_json(train_csv_path,
+	                             args.video_path, args.video_type, args.dst_path)
